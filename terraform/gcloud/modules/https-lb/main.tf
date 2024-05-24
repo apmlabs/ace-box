af70cdde0c75bd6679cf2ad02d45ad554cab0902
apmlabs <54629140+apmlabs@users.noreply.github.com>
2024-05-24 07:22:28 +0200

    Feature/614/argo cd (#633)
    * Add initial resources
    
    * Add dashboard section
    
    * Add ArgoCD application set tasks
    
    * Fix app set name
    
    * Fix readme
    
    * Fix gitea fact names
    
    * Add k9s role
    
    * Allow all GitLab scopes
    
    * Add http load balancer resources
    
    * Remove temp comment
    
    * Parameterize namespace
    
    * Add default sync policy

--- terraform/gcloud/modules/https-lb/main.tf
+++ terraform/gcloud/modules/https-lb/main.tf
@@ -108,3 +108,17 @@ resource "google_compute_global_forwarding_rule" "ace_box_https" {
   target                = google_compute_target_https_proxy.ace_box.id
   ip_address            = google_compute_global_address.ace_box.address
 }
+
+resource "google_compute_target_http_proxy" "ace_box" {
+  name    = "${var.name_prefix}-http-${var.random_id}"
+  url_map = google_compute_url_map.ace_box.id
+}
+
+resource "google_compute_global_forwarding_rule" "ace_box_http" {
+  name                  = "${var.name_prefix}-http-${var.random_id}"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "80-80"
+  target                = google_compute_target_http_proxy.ace_box.id
+  ip_address            = google_compute_global_address.ace_box.address
+}
